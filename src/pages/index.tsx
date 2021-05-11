667c6d4e74906677d0232c7152978d93e83863a7
Gosch <ricardo.gosch@argoseguros.com.br>
2021-05-11 17:31:51 -0300

    Upgrade packages and install jest

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+import MainTemplate from 'templates/MainTemplate';
+
+const Page = () => {
+  return (
+    <>
+      <Head>
+        <title>NextJS Boilerplate</title>
+      </Head>
+      <MainTemplate />
+    </>
+  );
+};
+
+export default Page;
---
+++ src/templates/MainTemplate/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles';
+
+const MainTemplate = () => {
+  return (
+    <S.Container>
+      <h1>Main</h1>
+    </S.Container>
+  );
+};
+
+export default MainTemplate;
