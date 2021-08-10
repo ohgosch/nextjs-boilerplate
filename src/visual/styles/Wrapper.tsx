a6081605dc796ea3c22046ac4745323caf6c0938
Gosch <gosch@gosch.io>
2021-08-10 01:08:31 -0300

    Fix Wrapper resolution import

--- src/visual/styles/Wrapper.tsx
+++ src/visual/styles/Wrapper.tsx
@@ -1,26 +1,28 @@
 import styled, { css } from 'styled-components';
+import { rem } from 'polished';
 
 import { mobile, tablet, desktop } from 'visual/medias';
-import { RESOLUTIONS } from 'visual/constants';
 
 export const Wrapper = styled.div`
-  margin-left: auto;
-  margin-right: auto;
+  ${({ theme }) => css`
+    margin-left: auto;
+    margin-right: auto;
 
-  ${mobile(css`
-    width: 90%;
-    max-width: ${RESOLUTIONS.mobile.max};
-  `)}
+    ${mobile(css`
+      width: 90%;
+      max-width: ${rem(theme.resolutions.mobile.max)};
+    `)}
 
-  ${tablet(css`
-    width: 88%;
-    max-width: ${RESOLUTIONS.tablet.max};
-  `)}
-
-  ${desktop(css`
-    width: 90%;
-    max-width: ${RESOLUTIONS.desktop.min};
-  `)}
+    ${tablet(css`
+      width: 88%;
+      max-width: ${rem(theme.resolutions.tablet.max)};
+    `)}
+  
+    ${desktop(css`
+      width: 90%;
+      max-width: ${rem(theme.resolutions.desktop.min)};
+    `)}
+  `}
 `;
 
 export default Wrapper;
